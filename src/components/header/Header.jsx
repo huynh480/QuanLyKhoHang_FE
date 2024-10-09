1c8b8690d520071c2e8ae664da3bdbd5165e15e1
Trần Văn Huynh <106756683+huynh480@users.noreply.github.com>
2024-10-09 23:10:35 +0700

    man hinh OTP chua fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,19 @@
+
 /* eslint-disable */
 import React from 'react'
 
 import './Header.css';
 const Header = () => {
   return (
-    <div className=''>
-
+    <div className='heder'>
+      <div className='lef'>
+          <div><i class="fa-regular fa-star"></i></div>
+          <div><p>Công ty ABC</p></div>
+      </div>
+      <div className='right'>
+          <div><i class="fa-regular fa-bell"></i></div>
+          <div><i class="fa-solid fa-right-from-bracket"></i></div>
+      </div>
     </div>
   )
 }
--- src/pages/auth/confirmOTP/ConfirmOTP.jsx
+++ src/pages/auth/confirmOTP/ConfirmOTP.jsx
@@ -9,11 +9,12 @@ const ConfirmOTP = () => {
     <>
     <Header/>
     <div className="otp-container">
-      <img src="https://img.icons8.com/fluency/100/000000/new-post.png" alt="Email Icon" />
+      <img src="src/assets/images/images.png" alt="Email Icon" />
       <h2>Mã OTP gồm 6 chữ số đã được gửi đến bạn qua email</h2>
       <input type="text" maxLength="6" placeholder="######" />
-      <button>Tiếp</button>
       <p>Gửi lại OTP</p>
+      <button>Tiếp</button> 
+  
     </div>
 
     </>
